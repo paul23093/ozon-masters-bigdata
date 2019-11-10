771e6b6f5ff84ff457c1b6b52313e871d87bcc1c
Pavel Semenov <paul23093@yandex.ru>
2019-11-10 17:23:32 +0000

    fixed something

--- projects/1/filter_cond.py
+++ projects/1/filter_cond.py
@@ -3,10 +3,10 @@ def filter_cond(line_dict):
     Takes a dict with field names and values as the argument
     Returns True if conditions are satisfied
     """
-# try:    
-    cond_match = (
-       (int(line_dict["if1"]) > 20) & (int(line_dict["if1"]) < 40)
-    ) 
-    return True if cond_match else False
-# except ValueError:
-#     return False
+    try:    
+        cond_match = (
+           (int(line_dict["if1"]) > 20) & (int(line_dict["if1"]) < 40)
+        ) 
+        return True if cond_match else False
+    except ValueError:
+        return False
