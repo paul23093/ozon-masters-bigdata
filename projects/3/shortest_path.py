da945a7aa11a0e8f277c1f6fbcf5d2cbd790f2cd
Pavel Semenov <paul23093@yandex.ru>
2019-11-25 22:17:29 +0000

    fix

--- projects/3/shortest_path.py
+++ projects/3/shortest_path.py
@@ -1,10 +1,31 @@
+import os
+import sys
+
+SPARK_HOME = "/usr/hdp/current/spark2-client"
+PYSPARK_PYTHON = "/opt/conda/envs/dsenv/bin/python"
+os.environ["PYSPARK_PYTHON"]= PYSPARK_PYTHON
+os.environ["SPARK_HOME"] = SPARK_HOME
+
+PYSPARK_HOME = os.path.join(SPARK_HOME, "python/lib")
+sys.path.insert(0, os.path.join(PYSPARK_HOME, "py4j-0.10.7-src.zip"))
+sys.path.insert(0, os.path.join(PYSPARK_HOME, "pyspark.zip"))
+
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+
+conf = SparkConf()
+conf.set("spark.jars.packages", "graphframes:graphframes:0.7.0-spark2.3-s_2.11")
+
+spark = SparkSession.builder.config(conf=conf).getOrCreate()
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+spark.sparkContext.setLogLevel('WARN')
+
 def shortest_path(v_from, v_to, df, max_path_length=10):
     from pyspark.sql.functions import col, concat, lit
-    schema = StructType(fields=[
-        StructField("c1", IntegerType()),
-        StructField("c0", IntegerType())
-    ])
-    graph = raw_graph.distinct().cache()
+    graph = df.distinct().cache()
     temp_df_c = graph.filter('c0 = {v_from}')
     i = 1
     output_columns = ['c0', lit(','), 'c1']
@@ -29,3 +50,12 @@ def shortest_path(v_from, v_to, df, max_path_length=10):
     .show(20, False)
     )
     temp_df_c.select("path").write.mode("overwrite").text("hw3_output")
+
+
+if __name__ == '__main__':
+    schema = StructType(fields=[
+        StructField("c1", IntegerType()),
+        StructField("c0", IntegerType())
+    ])
+    raw_graph = spark.read.csv(f"{sys.argv[3]}", schema=schema, sep='\t')
+    shortest_path(int(sys.argv[1]), int(sys.argv[2]), sys.argv[3])
